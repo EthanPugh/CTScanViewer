6e237c33fff07870f7f69712083f22a6a53f8d24
EthanPugh <ethanthomaspugh@hotmail.com>
2020-02-17 15:50:54 +0000

    Added Javadoc for Axis and Controller.

--- src/Axis.java
+++ src/Axis.java
@@ -1,3 +1,8 @@
+/**
+ * Enum representing the three axes of a 3D image.
+ *
+ * @author Ethan Pugh
+ */
 public enum Axis {
     X,
     Y,
--- src/Controller.java
+++ src/Controller.java
@@ -6,6 +6,11 @@ import javafx.scene.image.WritableImage;
 
 import java.io.IOException;
 
+/**
+ * Controller for the main window which displays various pieces of data from the CT scan.
+ *
+ * @author Ethan Pugh
+ */
 public class Controller {
 
     @FXML
@@ -18,10 +23,14 @@ public class Controller {
     private Button buttonMIPX, buttonMIPY, buttonMIPZ;
 
     private WritableImage imageX, imageY, imageZ;
-    private int sliceValX, sliceValY, sliceValZ;
-    private float scaleValX, scaleValY, scaleValZ;
+
     private Volume data;
 
+    /**
+     * Sets up the Volume ready for use and initialises the images, sliders and buttons to work with the data.
+     *
+     * @throws IOException If the file isn't found.
+     */
     @FXML
     private void initialize() throws IOException {
         data = new Volume("src/resources/CT_head.raw", 256, 256, 113);
@@ -30,6 +39,9 @@ public class Controller {
         setupButtons();
     }
 
+    /**
+     * Sets the ImageViews to the first slice of each axis.
+     */
     private void setupImages() {
         imageX = data.getSlice(0, Axis.X);
         imageY = data.getSlice(0, Axis.Y);
@@ -39,6 +51,11 @@ public class Controller {
         imageViewZ.setImage(imageZ);
     }
 
+    /**
+     * Sets up event handlers for each slider:
+     * Slice sliders will retrieve a new image based on their value;
+     * Scale sliders will send the currently displayed image to be scaled.
+     */
     private void setupSliders() {
         sliderSliceX.setOnMouseDragged(event -> {
             imageX = data.getSlice(sliderSliceX.valueProperty().intValue(), Axis.X);
@@ -70,6 +87,10 @@ public class Controller {
 
     }
 
+    /**
+     * Sets up event handlers for each MIP button:
+     * Each button retrieves the MIP for the specified axis.
+     */
     private void setupButtons() {
         buttonMIPX.setOnMouseClicked(event -> {
             imageX = data.getMIP(Axis.X);
