73667ae3a3c60d778a08215c0decb83e2f9158b9
Ethan <ethanthomaspugh@hotmail.com>
2020-03-01 00:09:59 +0000

    Added new bilinear interpolation code.

--- src/ImageManipulator.java
+++ src/ImageManipulator.java
@@ -12,6 +12,20 @@ import javafx.scene.paint.Color;
 public class ImageManipulator {
 
     /**
+     * Scale an image by a specified amount.
+     *
+     * @param oldImage The image to be scaled.
+     * @param scale    The amount to increase/decrease the size of the image.
+     * @return The same image but scaled accordingly.
+     */
+    public static WritableImage scale(WritableImage oldImage, final float scale) {
+        final float newWidth = (float) (oldImage.getWidth() * scale);
+        final float newHeight = (float) (oldImage.getHeight() * scale);
+
+        return ImageManipulator.resize(oldImage, newWidth, newHeight);
+    }
+
+    /**
      * Resize an image to a new specified width and height.
      *
      * @param oldImage  The image to be resized.
@@ -26,29 +40,32 @@ public class ImageManipulator {
         PixelWriter newImageW = newImage.getPixelWriter();
         PixelReader oldImageR = oldImage.getPixelReader();
 
+
         for (int x = 0; x < newWidth - 1; x++) {
             for (int y = 0; y < newHeight - 1; y++) {
-                float xNew = (x * oldWidth / newWidth);
-                float yNew = (y * oldHeight / newHeight);
-                Color col = oldImageR.getColor((int) xNew, (int) yNew);
-                newImageW.setColor(x, y, col);
+                float oldX = x * oldWidth / newWidth;
+                float oldY = y * oldHeight / newHeight;
+
+                int x0 = (int) Math.floor(oldX);
+                int y0 = (int) Math.floor(oldY);
+                int x1 = x0 + 1;
+                int y1 = y0 + 1;
+
+                if (x0 >= oldWidth - 1 || y0 >= oldHeight - 1) {
+                    newImageW.setColor(x, y, oldImageR.getColor((int) Math.floor(oldX),(int) Math.floor(oldY)));
+                } else {
+                    Color C00 = oldImageR.getColor(x0, y0);
+                    Color C11 = oldImageR.getColor(x1, y1);
+
+                    double CX = C00.getRed() + (C11.getRed() - C00.getRed()) * ((oldX - x0) / (x1 - x0));
+                    double CY = C00.getRed() + (C11.getRed() - C00.getRed()) * ((oldY - y0) / (y1 - y0));
+                    double col = CX + (CY - CX) * ((oldY - y0) / (y1 - y0));
+
+                    newImageW.setColor(x, y, Color.color(col, col, col, 1.0));
+                }
             }
         }
         return newImage;
-    }
-
-    /**
-     * Scale an image by a specified amount.
-     *
-     * @param oldImage The image to be scaled.
-     * @param scale    The amount to increase/decrease the size of the image.
-     * @return The same image but scaled accordingly.
-     */
-    public static WritableImage scale(WritableImage oldImage, final float scale) {
-        final float newWidth = (float) (oldImage.getWidth() * scale);
-        final float newHeight = (float) (oldImage.getHeight() * scale);
-
-        return ImageManipulator.resize(oldImage, newWidth, newHeight);
     }
 
     /**
@@ -75,6 +92,31 @@ public class ImageManipulator {
             }
         }
         return mip;
+    }
+
+    /**
+     * Resize an image to a new specified width and height.
+     *
+     * @param oldImage  The image to be resized.
+     * @param newWidth  The width of the new image/resized image.
+     * @param newHeight The height of the new image/resized image.
+     * @return The same image but resized.
+     */
+    public static WritableImage resizeNearest(WritableImage oldImage, final float newWidth, final float newHeight) {
+        final float oldWidth = (float) oldImage.getWidth();
+        final float oldHeight = (float) oldImage.getHeight();
+        WritableImage newImage = new WritableImage((int) (newWidth), (int) (newHeight));
+        PixelWriter newImageW = newImage.getPixelWriter();
+        PixelReader oldImageR = oldImage.getPixelReader();
+
+        for (int x = 0; x < newWidth - 1; x++) {
+            for (int y = 0; y < newHeight - 1; y++) {
+                float oldX = (x * oldWidth / newWidth);
+                float oldY = (y * oldHeight / newHeight);
+                newImageW.setColor(x, y, oldImageR.getColor((int) oldX, (int) oldY));
+            }
+        }
+        return newImage;
     }
 
 }
